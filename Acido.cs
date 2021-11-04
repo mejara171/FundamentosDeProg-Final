d29cbfd75adfb76ee34069c230afac7d2c933ba3
Tamayovski <93049809+Tamayovski@users.noreply.github.com>
2021-11-03 23:23:28 -0500

    Add files via upload

---
+++ Acido.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acido : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+    protected AudioSource death;
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        death = GetComponent<AudioSource>();
+    }
+}
